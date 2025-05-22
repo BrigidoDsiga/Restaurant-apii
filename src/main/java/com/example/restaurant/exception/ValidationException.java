596ea0f1d1d684e5019dfff4dfec504008cdd64f
Brígido Domingos Siga <103961163+BrigidoDsiga@users.noreply.github.com>
2025-05-22 12:13:21 -0300

    Update ValidationException.java

--- src/main/java/com/example/restaurant/exception/ValidationException.java
+++ src/main/java/com/example/restaurant/exception/ValidationException.java
@@ -1,7 +1,9 @@
 package com.example.restaurant.exception;
 
 public class ValidationException extends RuntimeException {
+
     public ValidationException(String message) {
         super(message);
     }
+
 }
